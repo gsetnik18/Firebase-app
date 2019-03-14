995267d72ae2107179dcb3614fa7519e429d97ce
Gregory Setnik <gsetnik18@gmail.com>
2019-03-14 13:41:59 -0400

    added table and firebase reference and initialize

---
+++ assets/app.js
@@ -0,0 +1,14 @@
+var record = 0;
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyA-GUx34_JovDo9Jy2VbtXVDHfiFRQGvcY",
+    authDomain: "train-schedule-c00f5.firebaseapp.com",
+    databaseURL: "https://train-schedule-c00f5.firebaseio.com",
+    projectId: "train-schedule-c00f5",
+    storageBucket: "train-schedule-c00f5.appspot.com",
+    messagingSenderId: "834612099533"
+  };
+  firebase.initializeApp(config);
+
+  
